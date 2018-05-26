9046fff87cc8451942161cf7fb7070c1e35795fe
John Wolfe <jwolfe890@gmail.com>
2018-05-26 18:07:26 -0400

    includes in php admin

--- includes/navigation.php
+++ includes/navigation.php
@@ -30,15 +30,17 @@
                     ?>
 
 
-                    <!-- <li>
-                        <a href="#">About</a>
-                    </li>
                     <li>
+                        <a href="admin">Admin</a>
+                    </li>
+
+
+                    <!-- <li>
                         <a href="#">Services</a>
                     </li>
                     <li>
                         <a href="#">Contact</a>
-                    </li> -->
+                    </li> --> 
 
 
                 </ul>
